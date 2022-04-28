300fc9eeccb92be47a13c8fafa339f79b13be525
aluno <aluno@labinfo>
2022-04-28 20:10:19 -0300

    Aula salão

---
+++ https:/github.com/RosanaMacedo/salao.git/Salao/src/main/java/br/com/ifce/Salao/model/cliente.java
@@ -0,0 +1,42 @@
+package br.com.ifce.Salao.model;
+
+public class cliente {
+
+	private String nome;
+	private String cpf;
+	private int idade;
+	private String endereço;
+	private String telefone;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEndereço() {
+		return endereço;
+	}
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
